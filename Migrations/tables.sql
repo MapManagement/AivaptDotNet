bc9a0878640f2fa7a225162d1bd67bb511524f4a
Jan <MapManagement@protonmail.com>
2023-01-22 22:04:44 +0100

    Replace existing migration SQL script with EF core generated one

--- Migrations/tables.sql
+++ Migrations/tables.sql
@@ -1,31 +1,45 @@
-CREATE TABLE simple_command (
-  name varchar(16) NOT NULL,
-  command_text text DEFAULT NULL,
-  title varchar(24) DEFAULT NULL,
-  active tinyint(1) NOT NULL,
-  creator_id bigint(20) UNSIGNED NOT NULL,
-  color varchar(7) NOT NULL DEFAULT '#1ABC9C',
-  PRIMARY KEY (name)
-);
+CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+    `MigrationId` varchar(150) CHARACTER SET utf8mb4 NOT NULL,
+    `ProductVersion` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
+    CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
+) CHARACTER SET=utf8mb4;
 
-CREATE TABLE role (
-  role_id bigint(20) UNSIGNED NOT NULL,
-  guild_id bigint(20) UNSIGNED NOT NULL,
-  mod_permissions tinyint(1) NOT NULL,
-  PRIMARY KEY (role_id,guild_id)
-);
+START TRANSACTION;
 
-CREATE TABLE guild (
-  id bigint(20) UNSIGNED NOT NULL,
-  name varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  owner_id bigint(20) UNSIGNED NOT NULL,
-  PRIMARY KEY (id)
-);
+ALTER DATABASE CHARACTER SET utf8mb4;
 
-CREATE TABLE quote (
-	id BIGINT UNSIGNED auto_increment NOT NULL,
-	user_id BIGINT UNSIGNED NOT NULL,
-	text varchar(400) NULL,
-	created_at DATETIME NOT NULL,
-  PRIMARY KEY (id)
-);
+CREATE TABLE `guild` (
+    `Id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `Name` longtext CHARACTER SET utf8mb4 NOT NULL,
+    `OwnerId` bigint unsigned NOT NULL,
+    CONSTRAINT `PK_guild` PRIMARY KEY (`Id`)
+) CHARACTER SET=utf8mb4;
+
+CREATE TABLE `quote` (
+    `Id` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `UserId` bigint unsigned NOT NULL,
+    `Text` longtext CHARACTER SET utf8mb4 NOT NULL,
+    `CreatedAt` datetime(6) NOT NULL,
+    CONSTRAINT `PK_quote` PRIMARY KEY (`Id`)
+) CHARACTER SET=utf8mb4;
+
+CREATE TABLE `role` (
+    `RoleId` bigint unsigned NOT NULL AUTO_INCREMENT,
+    `GuildId` bigint unsigned NOT NULL,
+    `ModPermissions` tinyint(1) NOT NULL,
+    CONSTRAINT `PK_role` PRIMARY KEY (`RoleId`)
+) CHARACTER SET=utf8mb4;
+
+CREATE TABLE `simple_command` (
+    `Name` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+    `Text` longtext CHARACTER SET utf8mb4 NULL,
+    `Active` tinyint(1) NOT NULL,
+    `CreatorId` bigint unsigned NOT NULL,
+    `Color` longtext CHARACTER SET utf8mb4 NOT NULL DEFAULT ('#1ABC9C'),
+    CONSTRAINT `PK_simple_command` PRIMARY KEY (`Name`)
+) CHARACTER SET=utf8mb4;
+
+INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
+VALUES ('20230121210207_InitialCreate', '6.0.7');
+
+COMMIT;
