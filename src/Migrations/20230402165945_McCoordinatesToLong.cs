e0695444e64ea4a30df9ec5d330600931bbef4f0
Jan <MapManagement@protonmail.com>
2023-04-02 19:19:57 +0200

    Fix type of Minecraft coordinates, add database migration

---
+++ src/Migrations/20230402165945_McCoordinatesToLong.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AivaptDotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class McCoordinatesToLong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "Z",
+                table: "mc_coordinates",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "bigint unsigned");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Y",
+                table: "mc_coordinates",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "bigint unsigned");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "X",
+                table: "mc_coordinates",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(ulong),
+                oldType: "bigint unsigned");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<ulong>(
+                name: "Z",
+                table: "mc_coordinates",
+                type: "bigint unsigned",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<ulong>(
+                name: "Y",
+                table: "mc_coordinates",
+                type: "bigint unsigned",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<ulong>(
+                name: "X",
+                table: "mc_coordinates",
+                type: "bigint unsigned",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
